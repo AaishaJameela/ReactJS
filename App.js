124b5cc6784fc9e2b2f78a8621c25afd5d06de0a
AaishaJameela <109809658+AaishaJameela@users.noreply.github.com>
2022-08-08 13:12:24 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,25 +1,28 @@
-import React, {useState} from 'react';
+import React from 'react';
+import {Routes, Route, Link} from 'react-router-dom';
+import Home from './Home.js';
+import About from './About.js';
 import Contact from'./Contact.js';
 
-const App = () => {
-  const newData= "Header";
-  const [click, setClick] = useState(0)
-  
-  return (
-    //<div>
-    //<Header />
-    //<Sidebar />
-    //<div>Hi, welcome to this page!</div>
-    //<div><img src={pic} /></div>
-    //<Footer />
-    //</div>
 
-    <center><div>
-      <Contact data={newData} />
-      <p>You have clicked {click} times.</p>
-    <button onClick = {() => setClick(click + 1)}>Increment</button>
-    <button onClick = {() => setClick(click - 1)}>Decrement</button>
-    </div></center>
+const App = () => { 
+  return (
+    <div>
+     
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/About">About</Link></li>
+      <li><Link to="/Contact">Contact Us</Link></li>
+    </ul>
+
+    <Routes>
+      <Route path='/' element= {<Home />} />
+      <Route path='/About' element= {<About />} />
+      <Route path='/Contact' element= {<Contact />} />
+    </Routes>
+
+    </div>
+
   )
   }
 
